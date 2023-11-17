4d26920700a25ea7472bbeab88866deefa5c79ea
Yash <120245614+yashNiwane@users.noreply.github.com>
2023-11-18 01:06:07 +0530

    update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import re
 from flask import Flask, render_template, request
 from pytube import YouTube
-
+import os
 
 app = Flask(__name__)
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    video_title = None
+
     if request.method == 'POST':
         url = request.form['url']
         youtube = YouTube(url)
@@ -14,16 +19,11 @@ def index():
         video = youtube.streams.filter(file_extension='mp4', progressive=True).order_by('resolution').desc().first()
         
         # Download the video with audio
-        video.download('./downloads')
+        video_title = youtube.title
+        download_path = os.path.join('static', 'downloads')
+        video.download(download_path)
 
-        return """
-            <script>
-                alert('Video downloaded!');
-                window.location.href='/';
-            </script>
-        """
-    else:
-        return render_template('index.html')
+    return render_template('index.html', video_title=video_title)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
