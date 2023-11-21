5e0b58c36efe50f13c97609571e0cc1a19345bdc
Yash <120245614+yashNiwane@users.noreply.github.com>
2023-11-21 23:26:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,24 @@ app = Flask(__name__)
 def index():
     video_title = None
 
+
+
     if request.method == 'POST':
         url = request.form['url']
         youtube = YouTube(url)
         
+
         # Get the highest resolution video stream with audio
         video = youtube.streams.filter(file_extension='mp4', progressive=True).order_by('resolution').desc().first()
         
+
         # Download the video with audio
         video_title = youtube.title
         download_path = os.path.join('static', 'downloads')
         video.download(download_path)
+        
+
 
     return render_template('index.html', video_title=video_title)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
